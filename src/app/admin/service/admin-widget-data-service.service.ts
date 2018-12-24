4978059947e5dadb51bdf8d6d700b3064e1228d1
Sasitha Madushanka <sasitha@tunclab.com>
2018-12-24 08:25:53 +0530

    fixed post error

--- src/app/admin/service/admin-widget-data-service.service.ts
+++ src/app/admin/service/admin-widget-data-service.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Http, Response} from "@angular/http";
-import {Observable} from "rxjs";
-
+import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { map} from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 @Injectable()
 export class AdminWidgetDataServiceService {
 
   constructor(private http: Http) { }
 
 
-  public query(endPoint: string, query:string):Observable<any> {
+  public query(endPoint: string, query: string): Observable<any> {
     return this.http
       .post(endPoint, query)
       .map(this.extractData)
@@ -17,7 +19,7 @@ export class AdminWidgetDataServiceService {
 
 
   private extractData(res: Response) {
-    let body = res.json();
+    const body = res.json();
     return body || {};
   }
   private handleErrorObservable (error: Response | any) {
